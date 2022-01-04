5bf8c2416d1878ba808f62e648c880ccca72702d
girrija <girrija@DESKTOP-61HEHPO>
2022-01-04 16:10:45 +0530

    added testscript TC_003

---
+++ JenkinsDemo/src/test/java/TC_003Test.java
@@ -0,0 +1,11 @@
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC_003Test {
+	
+	@Test
+	public void Account() {
+		Reporter.log("run TestScript 3",true);
+	}
+
+}
