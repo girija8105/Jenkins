af885c83a3d83f6df89e95cb3f7b6f1dbf994b75
girrija <girrija@DESKTOP-61HEHPO>
2022-01-04 12:35:07 +0530

    added to jenkins

---
+++ JenkinsDemo/src/test/java/TC_001Test.java
@@ -0,0 +1,24 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TC_001Test {
+	
+	@Test
+	public void createAccount() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.get("https://www.selenium.dev/downloads/");
+		Reporter.log("open the application");
+		
+	}
+
+}
