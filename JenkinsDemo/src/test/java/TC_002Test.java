9c0585fbf0dfb7fd45fb9dc4f5e163cbdab00fb8
girrija <girrija@DESKTOP-61HEHPO>
2022-01-04 15:40:17 +0530

    added new testScript TC_002

---
+++ JenkinsDemo/src/test/java/TC_002Test.java
@@ -0,0 +1,11 @@
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC_002Test {
+	
+	@Test
+	public void myTest() {
+		Reporter.log("run my testscript",true);
+	}
+
+}
